809525f7ec6365f511f085b15b180005a9341ab9
Narawich Youbua <narawich49@gmail.com>
2022-04-02 23:11:41 +0700

    add SoftStu to classdoc and update Activity AJAX with .net ASP MVC

---
+++ Semister 3-2/SoftStu/Calculator465/Calculator465/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Calculator465.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Data;
+
+namespace Calculator465.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Produces("text/html")]
+        public IActionResult Calculate(string number)
+        {
+            CalculatorModel cal = new CalculatorModel();
+            string value = new DataTable().Compute(number, null).ToString();
+            cal.CalculateValue = value;
+            return Content(cal.CalculateValue, "text/html", Encoding.UTF8);
+        }
+    }
+}
