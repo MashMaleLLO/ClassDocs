e5d1567cf76a36a2b54404df58a933194a1e2c09
Narawich Youbua <narawich49@gmail.com>
2022-04-03 22:57:19 +0700

    Fin calculator

--- Semister 3-2/SoftStu/Calculator465/Calculator465/Calculator465/Calculator465/Controllers/HomeController.cs
+++ Semister 3-2/SoftStu/Calculator465/Calculator465/Calculator465/Calculator465/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
